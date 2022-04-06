7a60b28cb0729ca4fe8750b15a77dd555aa64dc9
alexis <alexisd.horton@gmail.com>
2022-04-06 17:12:57 -0400

    Added Services

---
+++ repos/angular-tour-of-heros/src/app/messages/messages.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { AppComponent } from '../app.component';
+import { HeroesComponent } from '../heroes/heroes.component';
+import { MessageService } from '../message.service';
+
+@Component({
+  selector: 'app-messages',
+  templateUrl: './messages.component.html',
+  styleUrls: ['./messages.component.css']
+})
+export class MessagesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
